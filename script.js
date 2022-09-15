e96338bf67df1d2c658e8255cdebce5aa62a54c9
Ihor <107474617+IhorOzerov@users.noreply.github.com>
2022-09-15 14:11:38 +0300

    Update and rename JS.js to script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const cats = [
     {
-        id: "firelover",
+        id: "Firelover",
         text: "This cat loves to chat a lot. He speaks even when asked to be quiet by the older cats. Will always find a way to say something. He loves to joke around and be nice. But despite his playfulness, he is lonely without a friend by his side. He really wants to communicate with a cat-girl. Beautiful cat-girl. You must be wondering why his name is Fire Lover? Well, I don't know myself, let's ask him next time you see him.",
         img: "./img/4.png",
         bodyColor: "#f4dec2",
         navColor:  "#9577f0"
     },
     {
-        id: "possum",
+        id: "Possum",
         text: "Possum cat? Yes, we have such a character. He is also a sweet and kind cat, and also smart. Much smarter than Fire Lover. He can meow in a made-up language that almost no one knows. He loves his opossum brothers very much and often shares pictures of them with the kittens. His advice is always helpful. And when he sleeps no one knows. He helps all the kittens find their inner cat... even though he's a possum.",
         img: "./img/Opossum.webp",
         bodyColor: "antiquewhite",
@@ -15,7 +15,7 @@ const cats = [
         num: 'text1'
     },
     {
-        id: "wise",
+        id: "Wise",
         text: "Hmm. Although it has become less cozy and cute, you are out of danger. This cat is also smart, he solves problems quickly and with great interest. He prefers to work at night and stay in the dark, but this does not make him evil at all. This is a wise cat, but also snarky and a little sarcastic, because he is smarter than others. But despite this, the Wise Cat also helps all kittens solve their problems and find their inner cat. Okay, let's get out of here, he doesn't like to talk in vain and get distracted.",
         img: "./img/3.png",
         bodyColor: "black",
@@ -23,7 +23,7 @@ const cats = [
         num: 'text2'
     },
     {
-        id: "redhead",
+        id: "Redhead",
         text: "This is Red Head. She, like all kittens, immediately went to solve all cases at once, while not forgetting about beauty. Faced with problems, she does not despair, but copes with them, albeit with hard work, but how else? Sometimes helps other kittens by showing their mistakes and giving advice. Is she smart? I don't know, but I know for sure that by perseverance she gets excellent results.",
         img: "./img/5.png",
         bodyColor: "#e0e8ba",  
@@ -31,7 +31,7 @@ const cats = [
         num: 'text3'
     },
     {
-        id: "manul",
+        id: "Manul",
         text: "And this is me, Manul. Yes, it was I who told you about other cats and kittens. And I hope you liked it, especially those kittens I wrote about, and older cats will like it too. I am a simple kitten, like everyone else. I also like to chat sometimes and help other kittens in any way I can, because I started not so long ago as a Wise Cat or Possum. I don't always have the brains, perseverance, or creativity, but I keep going. Sometimes I stop to warm my paws. Hope to see you again!",
         img: "./img/7.jpg",
         bodyColor: "#ccc3f0",
@@ -44,11 +44,10 @@ const buttonContainer = document.querySelector('#buttons');
 const contentElement = document.querySelector('.content');
 
 buttonContainer.addEventListener('click', ({target}) => {
-  if (target.closest(".nav_bar")) {
     const content = cats.find((cat) => cat.id === target.id);
     contentElement.innerHTML = renderElement(content);
   }
-});
+);
 
 const bodyElem = document.querySelector("body");
 const navBar = document.querySelector("#side");
@@ -59,3 +58,12 @@ const renderElement = ({text, img, bodyColor, navColor, num}) => {
     return ` <img class="image" src="${img}"id="descr_img" alt="meow">
      <p class="text ${num}">${text}</p>`
 };
+const renderNav = () => {
+  cats.forEach((cat) => {
+    buttonContainer.insertAdjacentHTML(
+      "beforeend",
+      `<li id="${cat.id}" class="nav_list">${cat.id}</li>`
+    );
+  });
+};
+window.addEventListener("DOMContentLoaded", renderNav);
